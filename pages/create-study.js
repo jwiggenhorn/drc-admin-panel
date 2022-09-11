4fee680e910a1cc28e6889f94fdae60d99673319
Savannah Shaffer <sshaffe@siue.edu>
2022-09-11 16:37:33 -0500

    replaced text box with a select menu

--- pages/create-study.js
+++ pages/create-study.js
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import { post } from '@lib/utils'
 import { useRouter } from 'next/router'
-import { Button, TextField } from '@mui/material'
+import { Button, Select, TextField } from '@mui/material'
+import { InputLabel } from '@mui/material';
+import { FormControl } from '@mui/material';
 
 export default function CreateStudy() {
   const [title, setTitle] = useState('')
@@ -60,18 +62,38 @@ export default function CreateStudy() {
         error={!isValidParticipantLimit}
         helperText={isValidParticipantLimit ? '' : 'Must be between 1 and 500'}
       />
-      <TextField
-        id="outlined-basic"
-        label="Input profile"
-        variant="outlined"
-        type="number"
-        value={inputProfile}
-        onChange={(e) => setInputProfile(e.target.value)}
-        sx={{ mb: 4 }}
-        inputProps={{ min: 0, max: 16 }}
-        error={!isValidInputProfile}
-        helperText={isValidInputProfile ? '' : 'Must be between 0 and 16'}
-      />
+      <FormControl>
+        <InputLabel id="outlined-basic">Input Profile</InputLabel>
+        <Select
+          labelId="outlined-basic-label"
+          id="outlined-basic"
+          type="number"
+          value={inputProfile}
+          label="Input Profile"
+          onChange={(e) => setParticipantLimit(e.target.value)}
+          sx={{ mb: 4 }}
+          inputProps={{ min: 0, max: 16 }}
+          error={!isValidInputProfile}
+          helperText={isValidInputProfile ? '' : 'Must be between 0 and 16'}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+          <option value={7}>7</option>
+          <option value={8}>8</option>
+          <option value={9}>9</option>
+          <option value={10}>10</option>
+          <option value={11}>11</option>
+          <option value={12}>12</option>
+          <option value={13}>13</option>
+          <option value={14}>14</option>
+          <option value={15}>15</option>
+          <option value={16}>16</option>
+        </Select>
+      </FormControl>
       <br />
       <Button
         variant="contained"
