043148e0a3fc74b9aca15b71a37335aa92a74b83
Jonah <jonahwiggenhorn@gmail.com>
2022-10-11 14:26:49 -0500

    added upload button

--- pages/create-study.js
+++ pages/create-study.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { inputProfileNames, post } from '@lib/utils'
 import { useRouter } from 'next/router'
+import UploadIcon from '@mui/icons-material/FileUpload'
 import {
   Button,
   Select,
@@ -20,6 +21,7 @@ export default function CreateStudy() {
   const [participantLimit, setParticipantLimit] = useState(1)
   const [inputProfile, setInputProfile] = useState(0)
   const [joystickSensitivity, setJoystickSensitivity] = useState('low')
+  const [file, setFile] = useState()
   const router = useRouter()
   const isValidParticipantLimit =
     participantLimit >= 1 && participantLimit <= 500
@@ -59,59 +61,90 @@ export default function CreateStudy() {
         fullWidth
         sx={{ mb: 3 }}
       />
-      <TextField
-        label="Max number of participants"
-        variant="outlined"
-        type="number"
-        value={participantLimit}
-        onChange={(e) => setParticipantLimit(e.target.value)}
-        sx={{ mr: 3 }}
-        inputProps={{ min: 1, max: 500 }}
-        error={!isValidParticipantLimit}
-        helperText={isValidParticipantLimit ? '' : 'Must be between 1 and 500'}
-      />
-      <FormControl style={{ minWidth: 120 }}>
-        <InputLabel>Input Profile</InputLabel>
-        <Select
-          value={inputProfile}
-          label="Input Profile"
-          onChange={(e) => setInputProfile(e.target.value)}
-          sx={{ mb: 4, mr: 3 }}
-        >
-          {[...inputProfileNames].map((value) => (
-            <MenuItem key={value} value={value[0]}>
-              {value[1]}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      {joystickProfiles.includes(inputProfile) && (
-        <FormControl>
-          <FormLabel>Joystick Sensitivity</FormLabel>
-          <RadioGroup row>
-            <FormControlLabel
-              label="Low"
-              value="low"
-              control={
-                <Radio
-                  checked={joystickSensitivity === 'low'}
-                  onChange={() => setJoystickSensitivity('low')}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <span>
+          <TextField
+            label="Max number of participants"
+            variant="outlined"
+            type="number"
+            value={participantLimit}
+            onChange={(e) => setParticipantLimit(e.target.value)}
+            sx={{ mr: 3 }}
+            inputProps={{ min: 1, max: 500 }}
+            error={!isValidParticipantLimit}
+            helperText={
+              isValidParticipantLimit ? '' : 'Must be between 1 and 500'
+            }
+          />
+          <FormControl style={{ minWidth: 120 }}>
+            <InputLabel>Input Profile</InputLabel>
+            <Select
+              value={inputProfile}
+              label="Input Profile"
+              onChange={(e) => setInputProfile(e.target.value)}
+              sx={{ mb: 4, mr: 3 }}
+            >
+              {[...inputProfileNames].map((value) => (
+                <MenuItem key={value} value={value[0]}>
+                  {value[1]}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {joystickProfiles.includes(inputProfile) && (
+            <FormControl>
+              <FormLabel>Joystick Sensitivity</FormLabel>
+              <RadioGroup row>
+                <FormControlLabel
+                  label="Low"
+                  value="low"
+                  control={
+                    <Radio
+                      checked={joystickSensitivity === 'low'}
+                      onChange={() => setJoystickSensitivity('low')}
+                    />
+                  }
                 />
-              }
-            />
-            <FormControlLabel
-              label="High"
-              value="high"
-              control={
-                <Radio
-                  checked={joystickSensitivity === 'high'}
-                  onChange={() => setJoystickSensitivity('high')}
+                <FormControlLabel
+                  label="High"
+                  value="high"
+                  control={
+                    <Radio
+                      checked={joystickSensitivity === 'high'}
+                      onChange={() => setJoystickSensitivity('high')}
+                    />
+                  }
                 />
-              }
+              </RadioGroup>
+            </FormControl>
+          )}
+        </span>
+        {file ? (
+          <p>
+            <b>MP3: </b>
+            {file.name.slice(0, -4)}
+          </p>
+        ) : (
+          <FormControl>
+            <input
+              hidden
+              accept=".mp3"
+              type="file"
+              id="file-upload-button"
+              onChange={(e) => setFile(e.target.files[0])}
             />
-          </RadioGroup>
-        </FormControl>
-      )}
+            <label htmlFor="file-upload-button">
+              <Button
+                startIcon={<UploadIcon />}
+                variant="contained"
+                component="span"
+              >
+                Upload MP3
+              </Button>
+            </label>
+          </FormControl>
+        )}
+      </div>
       <br />
       <Button
         variant="contained"
