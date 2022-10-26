b2eb145727950122a1be16fb20025bc2c57c79b3
jwiggen <jwiggen@siue.edu>
2022-10-26 09:20:49 -0500

    added privacy policy and license

--- components/layout.js
+++ components/layout.js
@@ -1,19 +1,21 @@
 import Navbar from './navbar'
+import { Alert } from '@mui/material'
 import { Container } from '@mui/system'
 import { useSession } from 'next-auth/react'
-import { Alert } from '@mui/material'
+import { useRouter } from 'next/router'
 
 export default function Layout({ children }) {
   const { data: session } = useSession()
+  const router = useRouter()
+
   return (
     <>
       <Navbar />
-      {session && (
+      {session || router.route === '/privacy' ? (
         <main>
           <Container maxWidth="md">{children}</Container>
         </main>
-      )}
-      {!session && (
+      ) : (
         <Container maxWidth="sm">
           <Alert severity="warning">Please sign in to view this page.</Alert>
         </Container>
---
+++ pages/privacy.js
@@ -0,0 +1,20 @@
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        No personal identifiable information is collected from users of the DRC
+        Interface mobile application. The only data that is collected is the
+        state of various controls at relative timestamps. If the user interacts
+        with a control such as a joystick, button, or slider on screen within
+        the DRC Interface application, the state of that control will be
+        recorded along with the timestamp at which the interaction occurred
+        relative to when the user pressed the START button. After the user
+        completes their participation in the study by pressing the STOP button
+        and confirming their submission, this input data is anonymously and
+        securely sent directly to our backend. This input data can then only be
+        viewed by the creator and administrator of that particular study.
+      </p>
+    </div>
+  )
+}
